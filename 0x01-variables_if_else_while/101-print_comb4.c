563cee38f4a8a89ec2b211f3bf90a7cbbd53e1bb
Edwin Mwangi <edwindugire@gmail.com>
2023-07-14 19:02:15 -0700

    Task 12

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+int hundredsDigit, tensDigit, onesDigit;
+for (hundredsDigit = 0; hundredsDigit <= 7; hundredsDigit++)
+{
+for (tensDigit = hundredsDigit + 1; tensDigit <= 8; tensDigit++)
+{
+for (onesDigit = tensDigit + 1; onesDigit <= 9; onesDigit++)
+{
+putchar(hundredsDigit + '0');
+putchar(tensDigit + '0');
+putchar(onesDigit + '0');
+if (hundredsDigit != 7 || tensDigit != 8 || onesDigit != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+
+return 0;
+}
